76e3dcd6d8f83a895b78bd467836be13d939c73a
e-ekeruo <ealiotta3@gmail.com>
2024-11-16 14:08:00 -0800

    Add files via upload

---
+++ Rstudio_lab.R
@@ -0,0 +1,35 @@
+library (datasets)
+data(iris)
+View(iris)
+
+unique(iris$Species)
+
+print("Hello World!")
+
+2+2 #addition 
+
+4-2 #subtraction
+
+a <- 1 #assigning 1 to the varibale called a
+a + 2 #adding a
+
+df = data.frame(Emp_Name = c("Jai" , "David" , "Michael") , Job_role = c("Manager" , "Team Lead" , "Developer"))
+
+x <- 1
+y<-2
+z<-x+y
+print(z)
+
+x <- 3
+y <- 4
+z <- x + y
+print(z)
+
+x <- 10
+y <- 20
+print(y-x)
+
+x <- 10
+y <- 20
+result <- y-x
+print(result)
